b32adb32b58c54868dc638b28cbd710b8b86716a
u114264 <DS\u114264@54004p24xu.ds.upf.edu>
2018-03-22 18:48:34 +0100

    done

--- P2PLN/classification.py
+++ P2PLN/classification.py
@@ -13,8 +13,25 @@ class utils():
 		file.close()
 
 		return lines	
+	def parser(self, elem):
+		forbidden = ("?" , "!",'"', ",", ".", ";", ":")
+		for s in  forbidden:
+			if s =="." and re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", elem) != None:
+				break;
 
-class Classification():
+			elem =elem.replace(s,'')
+
+			#if elem is not in forbidden:	
+		elem = elem.lower()
+		return elem
+
+	def writeFile(self, fileName, data):
+
+		file = open(fileName, 'w')
+		file.write(data)
+		file.close()
+
+class Extraction():
 	def __init__ (self):
 		self.features = {}
 
@@ -34,15 +51,7 @@ class Classification():
 			#End of File
 			if elem == '':
 				break
-			forbidden = ("?" , "!",'"', ",", ".", ";", ":")
-			for s in  forbidden:
-				if s =="." and re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", elem) != None:
-					break;
-
-  				elem =elem.replace(s,'')
-
-			#if elem is not in forbidden:	
-			elem = elem.lower()
+			elem = utils().parser(elem)
 			#afegim paraula amb valor 1 si el diccionari no la conte
 			if not self.features.has_key(elem):
 				self.features[elem] = 1
@@ -51,24 +60,57 @@ class Classification():
 			else:
 				self.features[elem]+= 1
 
+class Classifier():
+	def __init__ (self, features):
+		self.features = features
+		self.data = ""
 
-
-	def toArffFormat(features):
-		data = "% 1. Title: Classification\n" +
-	    "% 2. Sources:\n"+
-	    "% (a) Creator: Núria Rodríguez, Eva Valls\n\n"+
-	    "@RELATION genre\n"
-	    for elem in features	
-	    	data += "@ATTRIBUTE "+elem +" STRING\n"
+	def toArffFormat(self):
+		data = "% 1. Title: Classification\n% 2. Sources:\n% (a) Creator: Nuria Rodriguez, Eva Valls\n\n@RELATION genre\n"
+		for elem in self.features:	
+			data += "@ATTRIBUTE "+elem +" STRING\n"
 	   	
-	    data+= "@ATTRIBUTE class        {female,male}"
+		data+= "@ATTRIBUTE class {female,male}\n@DATA\n"
+		self.data = data
+	def featureClassifier(self, dir):
+		dictionary = {}
+		self.toArffFormat()
+		for f in self.features:
+				dictionary[f] = 0
+
+		for filename in os.listdir(dir):
+			for elem in utils().getInputFile(dir+'/'+filename):
+				if elem == '':
+					break
+				elem = utils().parser(elem)
+				if dictionary.has_key(elem):
+					dictionary[elem]+=1
+
+			currentData = ""
+			for f in self.features:
+				if dictionary[f] == 0:
+					currentData+= "?,"
+				else :
+					currentData += str(dictionary[f]) + ","
+					dictionary[f] = 0
+			if "female" in filename:
+				currentData+= "female\n"
+			else:
+				currentData+= "male\n"
+			self.data += currentData
+		
+		utils().writeFile("classifier.arff",self.data)
+
 
 class Main():
 
 	#python tagger.py -t t1 -r results.txt 
-	classification = Classification()
-	features = classification.computeFeatures( "dataset" , 5, "words" )
-	classification.toArffFormat(features)
+	extraction = Extraction()
+	features = extraction.computeFeatures( "dataset" , 5, "words" )
+
+	classifier = Classifier(features)
+	classifier.featureClassifier("dataset")
+
 	"""test, solutions, result = "filename", "gold_standard_1.txt", "results.txt"
 	i = 0
 	for arg in sys.argv :
