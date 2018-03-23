a76cc81e30a2fcd66c5667f051ae4e99fa377814
u114256 <DS\u114256@54003p19xu.ds.upf.edu>
2018-03-23 19:49:49 +0100

    asfg

--- P2PLN/classification.py
+++ P2PLN/classification.py
@@ -13,8 +13,8 @@ class utils():
 		file.close()
 
 		return lines	
-	def parser(self, elem):
-		forbidden = ("?" , "!",'"', ",", ".", ";", ":", "-")
+	def parserWord(self, elem):
+		forbidden = ("?" , "!",'"', ",", ".", ";", ":", "-", "'")
 		for s in  forbidden:
 			if s =="." and re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", elem) != None:
 				break;
@@ -24,6 +24,20 @@ class utils():
 			elem =elem.replace(" ",'')
 			#if elem is not in forbidden:	
 		elem = elem.lower()
+		return elem
+
+	def parserPunctuation(self, elem):
+		punctuation = ("?" , "!",'"', ",", ".", ";", ":", "-")
+		
+		for s in  punctuation:
+			if s in elem:
+				elem = s
+			if s =="." and re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", elem) != None:
+				break;
+
+		if elem not in punctuation:
+			elem =elem.replace(elem,'')
+		
 		return elem
 
 	def writeFile(self, fileName, data):
@@ -52,48 +66,60 @@ class Extraction():
 			#End of File
 			if elem == '':
 				break
-			elem = utils().parser(elem)
+			if type=="word":
+				elem = utils().parserWord(elem)
+			elif type == "punct":
+				elem = utils().parserPunctuation(elem)
 			#afegim paraula amb valor 1 si el diccionari no la conte
-			if not self.features.has_key(elem):
-				if elem !="":
-					self.features[elem] = 1
+			if elem!="":
+				if not self.features.has_key(elem):
+					if elem !="":
+						self.features[elem] = 1
 
-			#actualitzem valors
-			else:
-				self.features[elem]+= 1
+				#actualitzem valors
+				else:
+					self.features[elem]+= 1
 
 class Classifier():
 	def __init__ (self, features):
 		self.features = features
 		self.data = ""
 
-	def toArffFormat(self):
+	def toArffFormat(self, type):
 		data = "% 1. Title: Classification\n% 2. Sources:\n% (a) Creator: Nuria Rodriguez, Eva Valls\n\n@RELATION genre\n"
 		for elem in self.features:	
-			data += "@ATTRIBUTE "+elem +" STRING\n"
+			if type== "punct":
+				elem = "'"+elem+"'"
+			data += "@ATTRIBUTE "+elem +" NUMERIC\n"
 	   	
 		data+= "@ATTRIBUTE class {female,male}\n@DATA\n"
 		self.data = data
-	def featureClassifier(self, dir):
+	def featureClassifier(self, dir, type):
 		dictionary = {}
-		self.toArffFormat()
+		
+		self.toArffFormat(type)
 		for f in self.features:
 				dictionary[f] = 0
 
 		for filename in os.listdir(dir):
+			count = 0.0
 			for elem in utils().getInputFile(dir+'/'+filename):
 				if elem == '':
 					break
-				elem = utils().parser(elem)
-				if dictionary.has_key(elem):
-					dictionary[elem]+=1
-
+				if type == "word":
+					elem = utils().parserWord(elem)
+				elif type == "punct":
+					elem = utils().parserPunctuation(elem)
+				if elem!="":
+					if dictionary.has_key(elem):
+						dictionary[elem]+=1
+					count += 1
 			currentData = ""
 			for f in self.features:
 				if dictionary[f] == 0:
 					currentData+= "?,"
 				else :
-					currentData += str(dictionary[f]) + ","
+					currentData += str(dictionary[f]/count) + ","
 					dictionary[f] = 0
 			if "female" in filename:
 				currentData+= "female\n"
@@ -106,29 +132,19 @@ class Classifier():
 
 class Main():
 
-	#python tagger.py -t t1 -r results.txt 
+	#python classification.py -n 100 -t word 
+
+	N, type = 100, "word"
+	i = 0
+	for arg in sys.argv:
+		if arg=="-n" :
+			N = int(sys.argv[i+1])
+		if arg=="-t":
+			type = sys.argv[i+1]
+		i+=1
+
 	extraction = Extraction()
-	features = extraction.computeFeatures( "dataset" , 30, "words" )
+	features = extraction.computeFeatures( "dataset" , N, type )
 
 	classifier = Classifier(features)
-	classifier.featureClassifier("dataset")
-
-	"""test, solutions, result = "filename", "gold_standard_1.txt", "results.txt"
-	i = 0
-	for arg in sys.argv :
-		if arg == "-t" :
-			if sys.argv[i+1] == "t1":
-				test = "test_1.txt"
-				solutions = "gold_standard_1.txt"
-			if sys.argv[i+1] == "t2":
-				test = "test_2.txt"
-				solutions = "gold_standard_2.txt"
-		if arg == "-r" :
-			results = sys.argv[i+1]
-		
-		i += 1
-	
-    
-    	punts extres -> calcular altres features com freq signes de puntuacio, etc
-    	weka-> classify-> cross validation
-	"""
+	classifier.featureClassifier("dataset", type)
